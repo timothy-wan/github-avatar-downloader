343e6ff8b48cd4d4552eb8e2eaf2ea30b2153bd1
tw5033 <tw5033@gmail.com>
2019-04-08 21:08:36 +0000

    step 4 of exercise completed

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,10 @@ var request = require('request');
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + '/contributors';
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
