ce2fab32f4ce111246ff7725900ec50de09011f1
tw5033 <tw5033@gmail.com>
2019-04-08 21:20:12 +0000

    step 6 completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,17 @@
 var request = require('request');
+var token = require('./secrets');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + '/contributors';
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request'
+    },
+    Authorization: token
+  }
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
@@ -13,3 +20,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
